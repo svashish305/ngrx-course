89b1a8312129c950ca1b12d0cf07551ce52e8d90
Shubhashish Verma <svashish305@gmail.com>
2019-09-26 22:32:28 +0530

    loaded with entity

--- src/app/courses/courses.resolver.ts
+++ src/app/courses/courses.resolver.ts
@@ -2,9 +2,10 @@ import { Injectable } from "@angular/core";
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { AppState } from "../reducers";
-import { Store } from "@ngrx/store";
-import { tap, first, finalize } from "rxjs/operators";
+import { Store, select } from "@ngrx/store";
+import { tap, first, finalize, filter } from "rxjs/operators";
 import { loadAllCourses } from "./course.actions";
+import { areCoursesLoaded } from "./courses.selectors";
 
 @Injectable()
 export class CoursesResolver implements Resolve<any> {
@@ -20,12 +21,14 @@ export class CoursesResolver implements Resolve<any> {
 
     return this.store
       .pipe(
-        tap(() => {        
-          if (!this.loading) {
+        select(areCoursesLoaded),
+        tap(coursesLoaded => {        
+          if (!this.loading && !coursesLoaded) {
             this.loading = true;
             this.store.dispatch(loadAllCourses());
           }
         }),
+        filter(coursesLoaded => coursesLoaded),
         first(),
         finalize(() => this.loading = false)
       );
--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,22 +1,28 @@
 import { EntityState, createEntityAdapter } from "@ngrx/entity";
-import { Course } from "../model/course";
+import { Course, compareCourses } from "../model/course";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
 
 export interface CoursesState extends EntityState<Course> {
-
+  allCoursesLoaded: boolean
 }
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses
+});
 
-export const initialCoursesState = adapter.getInitialState();
+export const initialCoursesState = adapter.getInitialState({
+  allCoursesLoaded: false
+});
 
 export const coursesReducer = createReducer(
   
   initialCoursesState,
 
   on(CourseActions.allCoursesLoaded,
-    (state, action) => adapter.addAll(action.courses, state))
+    (state, action) => adapter.addAll(action.courses,
+      {...state, allCoursesLoaded: true}
+    ))
 
 );
 
