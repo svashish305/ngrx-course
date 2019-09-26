c4210b856a04901fda48576464d138bc8a13ac84
Shubhashish Verma <svashish305@gmail.com>
2019-09-26 17:53:51 +0530

    added courses resolver

---
+++ src/app/courses/courses.resolver.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs";
+import { AppState } from "../reducers";
+import { Store } from "@ngrx/store";
+import { tap, first, finalize } from "rxjs/operators";
+import { loadAllCourses } from "./course.actions";
+
+@Injectable()
+export class CoursesResolver implements Resolve<any> {
+  
+  loading = false;
+
+  constructor(private store: Store<AppState>) {
+
+  }
+
+  resolve(route: ActivatedRouteSnapshot, 
+          state: RouterStateSnapshot): Observable<any> {
+
+    return this.store
+      .pipe(
+        tap(() => {        
+          if (!this.loading) {
+            this.loading = true;
+            this.store.dispatch(loadAllCourses());
+          }
+        }),
+        first(),
+        finalize(() => this.loading = false)
+      );
+  }
+}
